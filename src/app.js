7e44a6ed17cb82d2de68ec12ce0856c577528b0e
Matheus Rocha <01devmath@gmail.com>
2022-06-18 18:25:57 -0300

    Add dependencies modules

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const forecast = require('./utils/forecast');
+const ipstack = require('./utils/ipstack');
 
 const app = express();
 
@@ -9,6 +11,11 @@ app.use(express.static(PUBLIC_FOLDER));
 
 app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
+
+  ipstack((data) => {
+    console.log(data);
+  });
+
 });
 
 app.listen(8080, () => {
